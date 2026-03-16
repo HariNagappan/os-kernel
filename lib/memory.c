7ec71fd178b6b7347cfb1cc7ce9faff9496b728b
Lohith V <lohithvbaskar@gmail.com>
2026-03-17 01:22:31 +0530

    feat: added strlen, strcmp, strcpy and memcmp

--- lib/memory.c
+++ lib/memory.c
@@ -20,3 +20,47 @@ void* memset(void* dest, int value, size_t n)
 
     return dest;
 }
+
+size_t strlen(const char *str)
+{
+    size_t len = 0;
+
+    while (str[len])
+        len++;
+
+    return len;
+}
+
+int strcmp(const char *a, const char *b)
+{
+    while (*a && (*a == *b))
+    {
+        a++;
+        b++;
+    }
+
+    return *(unsigned char*)a - *(unsigned char*)b;
+}
+
+char *strcpy(char *dest, const char *src)
+{
+    char *d = dest;
+
+    while ((*d++ = *src++));
+
+    return dest;
+}
+
+int memcmp(const void *a, const void *b, size_t n)
+{
+    const unsigned char *p1 = a;
+    const unsigned char *p2 = b;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        if (p1[i] != p2[i])
+            return p1[i] - p2[i];
+    }
+
+    return 0;
+}
--- lib/memory.h
+++ lib/memory.h
@@ -5,5 +5,9 @@
 
 void* memcpy(void* dest, const void* src, size_t n);
 void* memset(void* dest, int value, size_t n);
+size_t strlen(const char *str);
+int strcmp(const char *a, const char *b);
+char *strcpy(char *dest, const char *src);
+int memcmp(const void *a, const void *b, size_t n);
 
 #endif
